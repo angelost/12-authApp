d1f7705ba3672586b41069d170f4ef9a8d599584
Angelo <angelo.gonzalez@live.com>
2022-09-30 15:36:46 -0300

    393. Mantener el usuario activo tras recargar

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, of, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
@@ -27,6 +27,7 @@ export class AuthService {
       .pipe(
         tap( resp => {
           if( resp.ok ) {
+            localStorage.setItem('token', resp.token! );
             this._usuario = {
               name: resp.name!,
               uid: resp.uid!
@@ -36,6 +37,14 @@ export class AuthService {
         map( resp => resp.ok),
         catchError(err => of(err.error.msg) )
       );
+  }
 
+  validarToken() {
+
+    const url = `${ this.baseUrl }/auth/renew`;
+    const headers = new HttpHeaders()
+     .set('x-token', localStorage.getItem('token') || '' );
+    
+    return this.http.get( url, { headers } );
   }
 }
