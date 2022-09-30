ef08df8a3bf40a68be7e8ef4e730fb2f9a396aee
Angelo <angelo.gonzalez@live.com>
2022-09-30 17:04:50 -0300

    399. Resolución de la tarea - Email

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -25,13 +25,9 @@ export class AuthService {
 
     return this.http.post<AuthResponse>(url, body)
       .pipe(
-        tap( resp => {
-          if( resp.ok ) {
-            localStorage.setItem('token', resp.token! );
-            this._usuario = {
-              name: resp.name!,
-              uid: resp.uid!
-            }
+        tap( ({ ok, token }) => {
+          if( ok ) {
+            localStorage.setItem('token', token! );            
           }          
         }),
         map( resp => resp.ok),
@@ -49,11 +45,7 @@ export class AuthService {
       .pipe(
         tap( resp => {
           if( resp.ok ) {
-            localStorage.setItem('token', resp.token! );
-            this._usuario = {
-              name: resp.name!,
-              uid: resp.uid!
-            }
+            localStorage.setItem('token', resp.token! );            
           }          
         }),
         map( resp => resp.ok),
@@ -74,7 +66,8 @@ export class AuthService {
           localStorage.setItem('token', resp.token! );
           this._usuario = {
             name: resp.name!,
-            uid: resp.uid!
+            uid: resp.uid!,
+            email: resp.email!
           }
 
           return resp.ok;
